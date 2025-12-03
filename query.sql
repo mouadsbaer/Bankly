d327febdfc8987ce3ab4e54565c008085b4b02e1
mouadsbaer <mouadsbaer@gmail.com>
2025-12-03 14:34:09 +0100

    Fix : Ajout de REQUÊTES AVEC FILTRES

--- query.sql
+++ query.sql
@@ -31,3 +31,44 @@ select account_number from accounts;
 -- 7. Affichage de toutes les transactions :
 select * from transactions;
 
+
+													-- SECTION 3 : REQUÊTES AVEC FILTRES
+                                                    
+-- 8. Affichage de tous les customers avec un balance > 10000 :
+select * from accounts
+where balance > 10000;
+
+-- 9. Affichage des accounts avec un balance ≤ 0 :
+select * from accounts
+where balance <= 0;
+
+-- 10. Affichage des transactions de type "debit" :
+select * from transactions
+where transaction_type = 'debit';
+
+-- 11. Affichage des transactions de type "credit" :
+ select * from transactions
+where transaction_type = 'credit';
+
+-- 12. Affichage des transactions du comptes qui le account_id = 1 :
+select * from transactions
+where account_id = 1;
+
+-- 13. Affichage des customers ayant un account géré par l’advisor_id = 2 :
+SELECT DISTINCT c.*
+FROM customer c
+JOIN accounts a ON c.customer_id = a.customer_id
+WHERE a.advisor_id = 2;
+
+-- 14. Affichage des comptes ayant account_type = "Savings"  :
+select * from accounts
+where account_type = "savings";
+
+-- 15. Affichage des transactions avec un amount ≥ 500:
+select * from transactions
+where amount >= 500;
+
+-- 16. Affichage des transactions avec un 100 <= amount <= 1000 :
+select * from transactions
+where amount > 100 and amount < 1000;
+
