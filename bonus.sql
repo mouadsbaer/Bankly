6beecd5bae5f5f72609eaf0ba9bd162f9a92ad0e
mouadsbaer <mouadsbaer@gmail.com>
2025-12-03 14:43:52 +0100

    Feat(bonus) : calcul et affichage du total du balance de tous les comptes de chaque client

--- bonus.sql
+++ bonus.sql
@@ -11,5 +11,10 @@ left join transactions t on a.account_id = t.account_id
 group by a.account_id, c.full_name, a.account_type
 order by account_id;
 
+-- Le total du balance de tous les comptes de chaque client :
+select c.customer_id, c.full_name , sum(a.balance) as total_balance
+from customer c
+join accounts a on c.customer_id = a.customer_id
+group by c.customer_id, c.full_name
+order by total_balance desc;
 
--- 
